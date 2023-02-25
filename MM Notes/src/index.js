8d56d30d94c20b0e1ed0d80421ea01c659980e24
mauriciomira1 <121832237+mauriciomira1@users.noreply.github.com>
2023-02-24 23:09:42 -0300

    mm notes 02

---
+++ MM Notes/src/index.js
@@ -0,0 +1,46 @@
+// Creating variables 
+const newTitle = document.getElementById('new-title')
+const newTitleInput = document.getElementById('new-title-input')
+const newNoteText = document.getElementById('new-note-text')
+const newNoteContainer = document.getElementById('new-note-container')
+const newNoteIcons = document.getElementById('new-note-container-icons')
+const newNoteItemIcons = document.getElementById('new-note-item-icons')
+const btnCloseNewNote = document.getElementById('new-note-container-button-close')
+const notesItens = document.getElementById('notes-itens')
+const newNoteItem = document.getElementById('new-note-item')
+
+
+// Starting functions area
+function changeContainerNew(ev) {
+  if (newNoteItem.parentElement.contains(ev.target)) {
+    newTitle.style.display = 'flex'
+    newNoteText.style.margin = '18px 0';
+    newNoteIcons.style.display = 'flex'
+    newNoteItemIcons.style.display = 'none'
+  } else {
+    newTitle.style.display = 'none'
+    newNoteIcons.style.display = 'none'
+    newNoteItemIcons.style.display = 'flex'
+  }
+}
+
+function cleanInputs() {
+  newTitleInput.value = ''
+  newNoteText.value = ''
+}
+
+function addNewNote() {
+  if (newTitleInput.value !== '' || newNoteText.value !== '') {
+    const note = newNoteContainer.cloneNode(true)
+    notesItens.appendChild(note)
+    cleanInputs()
+  } else {
+    changeContainerNew
+  }
+}
+
+
+
+// Starting Event Listener area
+btnCloseNewNote.addEventListener('click', addNewNote)
+window.addEventListener('click', changeContainerNew)
