d7caeb782521b688657cc454626e42afb7c2af37
mauriciomira1 <121832237+mauriciomira1@users.noreply.github.com>
2023-02-26 00:48:20 -0300

    mais mmnotes

--- MM Notes/src/index.js
+++ MM Notes/src/index.js
@@ -1,5 +1,5 @@
 // Creating variables 
-const newTitle = document.querySelector('new-title')
+const newTitle = document.querySelector('.new-title')
 const newTitleInput = document.getElementById('new-title-input')
 const newNoteText = document.getElementById('new-note-text')
 const newNoteContainer = document.getElementById('new-note-container')
@@ -13,16 +13,17 @@ const newNoteItem = document.getElementById('new-note-item')
 
 // Starting functions area
 function changeContainerNew(ev) {
+  if (ev.target.closest('#new-note-container-button-close')) {
+/*     addNewNote() */
+    return
+  }
   if (newNoteItem.parentElement.contains(ev.target)) {
     newTitle.style.display = 'flex'
     newNoteText.style.margin = '18px 0';
     newNoteIcons.style.display = 'flex'
     newNoteItemIcons.style.display = 'none'
-  } else {
-    newTitle.style.display = 'none'
-    newNoteIcons.style.display = 'none'
-    newNoteItemIcons.style.display = 'flex'
-  }
+    return
+  } else  addNewNote() 
 }
 
 function cleanInputs() {
@@ -30,21 +31,29 @@ function cleanInputs() {
   newNoteText.value = ''
   newTitle.style.display = 'none'
   newNoteIcons.style.display = 'none'
-  newNoteItemIcons.style.display = 'none'
+  newNoteItemIcons.style.display = 'flex'
 }
 
 function addNewNote() {
   if (newTitleInput.value !== '' || newNoteText.value !== '') {
-    const note = newNoteContainer.cloneNode(true)
+    let note = newNoteContainer.cloneNode(true)
     notesItens.appendChild(note)
+    note.classList.replace('new-note-container','note-container')
+    const title = note.querySelector('.new-title')
+    const noteText = note.querySelector('.new-note-item')
+    const icons = note.querySelector('.new-note-container-icons')
+    if (title.value === '') {title.style.display = 'none'}
+    else if (noteText.value === '') {
+      noteText.placeholder = '';
+    }
+    icons.style.display = 'none'
+    const newNoteItemIcons = note.querySelector('.new-shortnote-item-icons')
+    newNoteItemIcons.classList.remove('hide')
     cleanInputs()
   } else {
-    changeContainerNew
     cleanInputs()
   }
 }
-
-
 
 // Starting Event Listener area
 btnCloseNewNote.addEventListener('click', addNewNote)
